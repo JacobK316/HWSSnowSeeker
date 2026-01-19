d897375e5782cf2c5b34db7b06a6e93976fdabf7
Jacob Kappler <jacobkappler@gmail.com>
2026-01-18 21:51:24 -0500

    List of resorts added

--- SnowSeeker/Resort.swift
+++ SnowSeeker/Resort.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct Resort: Codable, Identifiable, Hashable {
+    var id: String
+    var name: String
+    var country: String
+    var description: String
+    var imageCredit: String
+    var price: Int
+    var size: Int
+    var snowDepth: Int
+    var elevation: Int
+    var runs: Int
+    var facilities: [String]
+    
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+}
