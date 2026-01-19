c8ff8c7db68ae01eb16e2b82316245265ffb5443
Jacob Kappler <jacobkappler@gmail.com>
2026-01-18 22:43:50 -0500

    added resort details

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct ResortView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    
+    let resort: Resort
+    
+    @State private var selectedFacility: Facility?
+    @State private var showingFacility = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack {
+                    if horizontalSizeClass == .compact && dynamicTypeSize > .large {
+                        VStack(spacing: 10) {
+                            ResortDetailsView(resort: resort)
+                        }
+                        VStack(spacing: 10) {
+                            SkiDetailsView(resort: resort)
+                        }
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                    }
+                }
+                .padding(.vertical)
+                .background(.primary.opacity(0.1))
+                
+                Group {
+                    HStack {
+                        ForEach(resort.facilityTypes) { facility in
+                            Button {
+                                selectedFacility = facility
+                                showingFacility = true
+                            } label: {
+                                facility.icon
+                                    .font(.title)
+                            }
+                        }
+                    }
+                    .padding(.vertical)
+                    
+                    Text("Description")
+                        .font(.headline)
+                    
+                    Text(resort.description)
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationTitle("\(resort.name), \(resort.country)")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert(selectedFacility?.name ?? "More information", isPresented: $showingFacility, presenting: selectedFacility) { _ in
+        } message: { facility in
+            Text(facility.description)
+        }
     }
 }
 
 #Preview {
-    ResortView()
+    ResortView(resort: .example)
 }
