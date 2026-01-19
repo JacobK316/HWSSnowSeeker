b7aaf2a13edb2185a763d5c7436571a6a9069b15
Jacob Kappler <jacobkappler@gmail.com>
2026-01-18 21:50:56 -0500

    List of resorts added

---
+++ SnowSeeker/Bundle-Decodable.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle-Decodable.swift
+//  SnowSeeker
+//
+//  Created by Jacob Kappler on 1/18/26.
+//
+
+import Foundation
---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,8 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Jacob Kappler on 1/18/26.
+//
+
+import Foundation
